d8ea2f08c09257c4383113c636021f24f950db86
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-03 11:26:41 +0200

    formatting

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,12 +1,11 @@
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
-import Data.Foldable     (for_)
-import Data.String       (fromString)
-import Test.Hspec        (Spec, describe, it, shouldBe)
-import Test.Hspec.Runner (configFailFast, defaultConfig, hspecWith)
-
+import Data.Foldable (for_)
+import Data.String (fromString)
 import Pangram (isPangram)
+import Test.Hspec (Spec, describe, it, shouldBe)
+import Test.Hspec.Runner (configFailFast, defaultConfig, hspecWith)
 
 main :: IO ()
 main = hspecWith defaultConfig {configFailFast = True} specs
@@ -14,68 +13,81 @@ main = hspecWith defaultConfig {configFailFast = True} specs
 specs :: Spec
 specs = describe "isPangram" $ for_ cases test
   where
-    test Case{..} = it description $ isPangram (fromString input) `shouldBe` expected
+    test Case {..} = it description $ isPangram (fromString input) `shouldBe` expected
 
-data Case = Case { description :: String
-                 , input       :: String
-                 , expected    :: Bool
-                 }
+data Case = Case
+  { description :: String,
+    input :: String,
+    expected :: Bool
+  }
 
 cases :: [Case]
-cases = [ Case { description = "with empty sentence"
-               , input       = ""
-               , expected    = False
-               }
-        , Case { description = "with perfect lower case"
-               , input       = "abcdefghijklmnopqrstuvwxyz"
-               , expected    = True
-               }
-        , Case { description = "with only lower case"
-               , input       = "the quick brown fox jumps over the lazy dog"
-               , expected    = True
-               }
-        , Case { description = "with missing character 'x'"
-               , input       = "a quick movement of the enemy will jeopardize five gunboats"
-               , expected    = False
-               }
-        , Case { description = "with missing character 'h'"
-               , input       = "five boxing wizards jump quickly at it"
-               , expected    = False
-               }
-        , Case { description = "with underscores"
-               , input       = "the_quick_brown_fox_jumps_over_the_lazy_dog"
-               , expected    = True
-               }
-        , Case { description = "with numbers"
-               , input       = "the 1 quick brown fox jumps over the 2 lazy dogs"
-               , expected    = True
-               }
-        , Case { description = "with missing letters replaced by numbers"
-               , input       = "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"
-               , expected    = False
-               }
-        , Case { description = "with mixed case and punctuation"
-               , input       = "\"Five quacking Zephyrs jolt my wax bed.\""
-               , expected    = True
-               }
-        , Case { description = "with mixed case"
-               , input       = "the quick brown fox jumps over with lazy FX"
-               , expected    = False
-               }
-        , Case { description = "with missing character and non-ascii letters"
-               , input       = "abcdefghijklmnopqrstuvwxyÃ"
-               , expected    = False
-               }
-        , Case { description = "with additional non-ascii letters"
-               , input       = "abcdefghijklmnopqrstuvwxyzÃ"
-               , expected    = True
-               }
-
-        {-
-        -- The following test can be enabled for String-based solutions:
-        , Case { description = "with termination as soon as all letters have occurred"
-               , input       = "abcdefghijklmnopqrstuvwxyz" ++ [undefined]
-               , expected    = True
-               }
-        -- -}
-        ]
+cases =
+  [ Case
+      { description = "with empty sentence",
+        input = "",
+        expected = False
+      },
+    Case
+      { description = "with perfect lower case",
+        input = "abcdefghijklmnopqrstuvwxyz",
+        expected = True
+      },
+    Case
+      { description = "with only lower case",
+        input = "the quick brown fox jumps over the lazy dog",
+        expected = True
+      },
+    Case
+      { description = "with missing character 'x'",
+        input = "a quick movement of the enemy will jeopardize five gunboats",
+        expected = False
+      },
+    Case
+      { description = "with missing character 'h'",
+        input = "five boxing wizards jump quickly at it",
+        expected = False
+      },
+    Case
+      { description = "with underscores",
+        input = "the_quick_brown_fox_jumps_over_the_lazy_dog",
+        expected = True
+      },
+    Case
+      { description = "with numbers",
+        input = "the 1 quick brown fox jumps over the 2 lazy dogs",
+        expected = True
+      },
+    Case
+      { description = "with missing letters replaced by numbers",
+        input = "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog",
+        expected = False
+      },
+    Case
+      { description = "with mixed case and punctuation",
+        input = "\"Five quacking Zephyrs jolt my wax bed.\"",
+        expected = True
+      },
+    Case
+      { description = "with mixed case",
+        input = "the quick brown fox jumps over with lazy FX",
+        expected = False
+      },
+    Case
+      { description = "with missing character and non-ascii letters",
+        input = "abcdefghijklmnopqrstuvwxyÃ",
+        expected = False
+      },
+    Case
+      { description = "with additional non-ascii letters",
+        input = "abcdefghijklmnopqrstuvwxyzÃ",
+        expected = True
+      }
+      {-
+      -- The following test can be enabled for String-based solutions:
+      , Case { description = "with termination as soon as all letters have occurred"
+             , input       = "abcdefghijklmnopqrstuvwxyz" ++ [undefined]
+             , expected    = True
+             }
+      -- -}
+  ]
